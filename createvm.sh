08d9a7a77cce30bf6f136592cde1097f773dd6eb
xupeng1231 <xupeng_1231@126.com>
2018-07-31 01:50:50 +0800

    createvm.sh added

---
+++ createvm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ ${#} -ne 3 ];
+then
+	echo "need 3 args!"
+	exit
+fi
+
+VNAME=${1} 
+TEMPVDI=${2}
+VRDEPORT=${3}
+DISKPATH="/home/xupeng/vm-disks/$VNAME.vdi"
+
+echo "copying vdi"
+vboxmanage clonehd ${TEMPVDI} ${DISKPATH}
+echo "copy finished"
+
+vboxmanage createvm --name ${VNAME} --ostype "Windows7_64" --register
+
+vboxmanage modifyvm ${VNAME} --memory 4096 --cpus 2
+
+vboxmanage storagectl ${VNAME} --name "IDE Controller" --add ide --bootable on
+
+vboxmanage storageattach ${VNAME} --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium ${DISKPATH} 
+
+vboxmanage modifyvm ${VNAME} --vrde on
+
+vboxmanage modifyvm ${VNAME} --vrdeport ${VRDEPORT}
+
+vboxmanage modifyvm ${VNAME} --vrdeaddress 192.168.4.2
+
+vboxmanage modifyvm ${VNAME} --vrdeauthtype null  
+
+vboxmanage modifyvm ${VNAME} --vrdeauthlibrary default
+
+#vboxmanage startvm ${VNAME} --type headless
+
